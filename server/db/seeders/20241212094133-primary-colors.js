bb57f7ecc8c7e992aba96ad1caff019b2d618b74
demirose41 <demirose41@gmail.com>
2024-12-12 01:48:31 -0800

    Completed practice

---
+++ server/db/seeders/20241212094133-primary-colors.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Colors', [{name: 'red'},{name: 'blue'},{name: 'yellow'}])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+      await queryInterface.bulkDelere('Colors', { name: ['red','blue','yellow']})
+  }
+};
